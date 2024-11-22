0246c2294ca171006a1cf4eda2c32ee6a7dd73b5
Naufal Maulana Malik <naufalmaulanamaliksr@gmail.com>
2024-11-22 07:28:06 +0700

    Initial Commit

---
+++ functions/scrape_data_from_tokopedia.py
@@ -0,0 +1,73 @@
+import time
+from selenium.webdriver.firefox.webdriver import WebDriver
+from bs4.__init__ import BeautifulSoup
+
+
+class ScrapeDataFromTokopedia:
+
+    def __init__(self, query):
+        self.scripts = None
+        self.query = query
+        self.soup = None
+        self.current_elements = None
+        self.current_element = None
+        self.temporary_elements = []
+        self.maximum_pages = 1000
+        self.page_start = 1
+        self.page_end = self.page_start + self.maximum_pages
+        self.page_range = range(self.page_start - 1, self.page_end)
+
+    def print_result(self):
+        self.current_element = self.soup.find("div", id="zeus-root")
+        self.soup = self.current_element
+
+        self.current_element = self.soup.find("div", class_="css-8atqhb")
+        self.soup = self.current_element
+
+        self.current_element = self.soup.find("div", class_="css-jau1bt")
+        self.soup = self.current_element
+
+        self.current_element = self.soup.find("div", class_="css-rjanld")
+        self.soup = self.current_element
+
+        self.current_element = self.soup.find(attrs={"data-testid": "divSRPContentProducts"})
+        self.soup = self.current_element
+
+        self.current_elements = self.soup.find_all("div", class_="css-jza1fo")
+        self.soup = self.current_elements
+        for element in self.soup:
+            sub_elements = element.find_all("div", class_="css-5wh65g")
+            # print(sub_elements)
+            for child_element in sub_elements:
+                child_element = child_element.find("a", class_="oQ94Awb6LlTiGByQZo8Lyw== IM26HEnTb-krJayD-R0OHw==")
+                # print(child_element)
+                if child_element is not None:
+                    class_name_for_normal_price = "_67d6E1xDKIzw+i2D2L0tjw== "
+                    class_name_for_current_price = "_67d6E1xDKIzw+i2D2L0tjw== t4jWW3NandT5hvCFAiotYg=="
+                    # class_name_for_previous_price = "q6wH9+Ht7LxnxrEgD22BCQ=="
+                    child_element = child_element.find("div", class_="bYD8FcVCFyOBiVyITwDj1Q==")
+                    product_name = child_element.find("span", class_="_0T8-iGxMpV6NEsYEhwkqEg==")
+                    normal_price = child_element.find("div", class_=class_name_for_normal_price)
+                    current_price = child_element.find("div", class_=class_name_for_current_price)
+                    sold_number = child_element.find("span", class_="se8WAnkjbVXZNA8mT+Veuw==")
+                    # previous_price = child_element.find("span", class_=class_name_for_previous_price)
+                    if normal_price is not None and current_price is None and sold_number is not None:
+                        self.temporary_elements.append([product_name.text, normal_price.text, sold_number.text])
+                        # print(product_name.text, normal_price.text)
+                    elif current_price is not None and normal_price is None and sold_number is not None:
+                        self.temporary_elements.append([product_name.text, current_price.text, sold_number.text])
+                        # print(product_name.text, current_price.text)
+
+    def search(self):
+        self.query = self.query.replace(" ", "+")
+        for m in range(self.maximum_pages):
+            driver = WebDriver()
+            driver.get("https://www.tokopedia.com/search?navsource=&page={}&q={}".format(m+1, self.query))
+            for k in range(0, 3000, 1000):
+                driver.execute_script(" window.scrollBy({}, {});".format(k, k+999))
+                time.sleep(3.)
+            time.sleep(2.)
+            self.scripts = driver.page_source
+            driver.close()
+            self.soup = BeautifulSoup(self.scripts, "html.parser")
+            self.print_result()
---
+++ main.py
@@ -0,0 +1,56 @@
+from functions.scrape_data_from_tokopedia import ScrapeDataFromTokopedia
+import csv
+
+contents = []
+extensive_contents = []
+
+scraper = ScrapeDataFromTokopedia("brankas")
+scraper.page_range = range(2, 18)
+scraper.search()
+
+for element in scraper.temporary_elements:
+    rows = []
+    for ele in element:
+        if ele.startswith("Rp"):
+            price = int(ele.removeprefix("Rp").replace(".", ""))
+            print(price, end=" ")
+            rows.append(price)
+        elif ele.endswith(" terjual"):
+            historical_selling = ele.removesuffix(" terjual")
+            if historical_selling.endswith("rb+"):
+                historical_selling = int(historical_selling.removesuffix("rb+")) * 1000
+                print(historical_selling, end=" ")
+                rows.append(historical_selling)
+            elif historical_selling.endswith("jt+"):
+                historical_selling = int(historical_selling.removesuffix("jt+")) * 1000000
+                print(historical_selling, end=" ")
+                rows.append(historical_selling)
+            elif historical_selling.endswith("+"):
+                historical_selling = int(historical_selling.removesuffix("+"))
+                print(historical_selling, end=" ")
+                rows.append(historical_selling)
+            else:
+                historical_selling = int(historical_selling)
+                print(historical_selling, end=" ")
+                rows.append(historical_selling)
+        else:
+            print(ele, end=" ")
+            rows.append(ele)
+    print()
+    contents.append(rows)
+
+
+try:
+    with open(f"resources/{scraper.query.replace("+", "_")}.csv", mode="x", newline="", encoding="UTF-8") as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(["Nama Produk", "Harga Produk", "Jumlah Barang Produk Yang Terjual"])
+        csv_writer.writerows(contents)
+except FileExistsError:
+    no_duplicate_contents = set(contents)
+    for content in contents:
+        if content not in no_duplicate_contents:
+            extensive_contents.append(content)
+
+    with open(f"resources/{scraper.query.replace("+", "_")}.csv", mode="a", newline="", encoding="UTF-8") as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerows(extensive_contents)
