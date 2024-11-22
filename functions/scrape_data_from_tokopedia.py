7257262081aa34ed31740ff335894bb80b1747ef
Naufal Maulana Malik <naufalmaulanamaliksr@gmail.com>
2024-11-22 13:52:49 +0700

    Remove step number and change variable declarations between line 79-80

--- functions/scrape_data_from_tokopedia.py
+++ functions/scrape_data_from_tokopedia.py
@@ -76,8 +76,8 @@ class ScrapeDataFromTokopedia:
             try:
                 driver = WebDriver()
                 driver.get("https://www.tokopedia.com/search?navsource=&page={}&q={}".format(m+1, self.query))
-                for k in range(0, 3000, 600):
-                    driver.execute_script(" window.scrollBy({}, {});".format(k, k+600))
+                for k in range(0, 3000):
+                    driver.execute_script(" window.scrollBy({}, {});".format(k, k+1))
                     time.sleep(0.05)
                 time.sleep(0.22)
                 self.scripts = driver.page_source
