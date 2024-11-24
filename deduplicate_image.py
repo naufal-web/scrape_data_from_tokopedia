2de68d6399ad26dd7c23f424e91a31c1ad1cd688
Naufal Maulana Malik <naufalmaulanamaliksr@gmail.com>
2024-11-24 08:01:34 +0700

    Adding new features

---
+++ deduplicate_image.py
@@ -0,0 +1,45 @@
+from os.path import join
+from os import listdir
+from os import getcwd
+import cv2
+
+
+def get_histogram_image_data(image):
+    image_data = cv2.imread(image)
+    try:
+        histo_image_data = cv2.calcHist([image_data], [0, 1, 2], None, [256, 256, 256],
+                                        [0, 256, 0, 256, 0, 256])
+        histo_image_data[255, 255, 255] = 0
+        cv2.normalize(histo_image_data, histo_image_data, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+        return histo_image_data
+    except cv2.error:
+        return None
+
+
+def get_similarity_score(histo_images):
+    image1, image2 = histo_images
+    try:
+        return cv2.compareHist(image1, image2, cv2.HISTCMP_CORREL)
+    except cv2.error:
+        return 0.0
+
+
+def deduplicate_images_from_directory(images_dir="images"):
+    images = [join(getcwd(), images_dir, image) for image in listdir(join(getcwd(), images_dir))]
+
+    images_set = set()
+
+    for i in range(len(images)):
+        for j in range(1, len(images)):
+            for k in range(2, len(images) + 1):
+                if len(images[i:j:k]) == 2:
+                    image_path_1, image_path_2 = images[i:j:k]
+                    histogram_images = get_histogram_image_data(image_path_1), get_histogram_image_data(image_path_2)
+                    score = get_similarity_score(histogram_images)
+                    if score == 1.0:
+                        images_set.add(image_path_1)
+
+    return images_set
+
+
+print(len(deduplicate_images_from_directory()))
--- generate_product.py
+++ generate_product.py
@@ -2,24 +2,58 @@ import os
 import csv
 from functions.scrape_data_from_tokopedia import ScrapeDataFromTokopedia
 
-root_path = os.getcwd()
-resources_dir = "resources"
-filename = "brankas"
-filenames = [filename + name for name in ["_terbaik", "_terbaru", "_terlaris", "_termurah"]]
 
-csv_extensions = ".csv"
+class UpdateResources(ScrapeDataFromTokopedia):
 
-for file_state in filenames:
-    query = file_state.replace("_", "+")
+    root_path = os.getcwd()
+    resources_dir = "resources"
+    filename = "brankas"
+    csv_extensions = ".csv"
 
-    saving_filepath = os.path.join(root_path, resources_dir, file_state + csv_extensions)
+    def retrieve_csv_file(self):
+        with open(self.savable_filepath, mode="r", newline="", encoding="UTF-8") as existing_filepath:
+            reader = csv.reader(existing_filepath)
+            existing_content = list(reader)[1:]
 
-    print(saving_filepath)
+        return existing_content
 
-    tokopedia = ScrapeDataFromTokopedia(query, 0, 28)
-    for content in tokopedia.temporary_elements:
-        print(content)
+    def get_new_data(self):
+        new_contents = []
+        for new_or_existed_content in self.temporary_elements:
+            if new_or_existed_content not in self.existing_content:
+                new_contents.append(new_or_existed_content)
 
-    with open(saving_filepath, mode="w", newline="", encoding="UTF-8") as saved_filepath:
-        file_writer = csv.writer(saved_filepath)
-        file_writer.writerows(tokopedia.temporary_elements)
+        return new_contents
+
+    def append_to_existing_data(self):
+        with open(self.savable_filepath, mode="a", newline="", encoding="UTF-8") as existing_filepath:
+            writer = csv.writer(existing_filepath)
+            writer.writerows(self.filtrated_content)
+
+    def creating_new_data(self):
+        with open(self.savable_filepath, mode="x", newline="", encoding="UTF-8") as new_filepath:
+            writer = csv.writer(new_filepath)
+            writer.writerows(self.temporary_elements)
+
+    def __init__(self, new_or_existed_query: str, start_index, end_index):
+        categories = ["_terbaik", "_terbaru", "_terlaris", "_termurah"]
+        self.filenames = [new_or_existed_query + name for name in categories]
+        if new_or_existed_query == self.filename:
+            for file in self.filenames:
+                super().__init__(query=file.replace(" ", "+"), page_start=start_index, page_end=end_index)
+                self.savable_filepath = os.path.join(self.root_path, self.resources_dir, file + self.csv_extensions)
+                self.existing_content = self.retrieve_csv_file()
+                self.filtrated_content = self.get_new_data()
+                self.append_to_existing_data()
+
+        else:
+            self.filename = new_or_existed_query
+            self.filenames.insert(0, self.filename)
+            for file in self.filenames:
+                super().__init__(query=file.replace(" ", "+"), page_start=0, page_end=end_index)
+
+                self.savable_filepath = os.path.join(self.root_path, self.resources_dir, file + self.csv_extensions)
+                self.creating_new_data()
+
+
+update_resources = UpdateResources(new_or_existed_query="mesin_bubut", start_index=0, end_index=13)
